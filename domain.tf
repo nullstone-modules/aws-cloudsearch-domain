b9fba06500ad8172f547ec8f6d4f6a7be47f745f
Scott Sickles <scottsickles@gmail.com>
2024-02-12 23:47:13 -0500

    fix the attribute name

--- domain.tf
+++ domain.tf
@@ -3,7 +3,7 @@ resource "aws_cloudsearch_domain" "this" {
 
   scaling_parameters {
     desired_instance_type = var.instance_type
-    desired_instance_count = var.instance_count
+    desired_replication_count = var.instance_count
     desired_partition_count = var.partition_count
   }
 }
