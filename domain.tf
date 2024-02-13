4325fea9d84ae786d0fcdd2f8c1f143103bf11d8
Scott Sickles <scottsickles@gmail.com>
2024-02-12 23:45:19 -0500

    add access policy

--- domain.tf
+++ domain.tf
@@ -7,3 +7,25 @@ resource "aws_cloudsearch_domain" "this" {
     desired_partition_count = var.partition_count
   }
 }
+
+data "aws_iam_policy_document" "search_suggest" {
+  statement {
+    sid    = "SearchSuggest"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "cloudsearch:search",
+      "cloudsearch:suggest",
+    ]
+  }
+}
+
+resource "aws_cloudsearch_domain_service_access_policy" "this" {
+  domain_name   = aws_cloudsearch_domain.this.id
+  access_policy = data.aws_iam_policy_document.search_suggest.json
+}
