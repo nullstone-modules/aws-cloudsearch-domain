25e5e465d04f726c6f75c8cb79199c94a24b2d2a
Scott Sickles <scottsickles@gmail.com>
2024-02-22 15:09:44 -0500

    set back to original

--- domain.tf
+++ domain.tf
@@ -7,8 +7,8 @@ resource "aws_cloudsearch_domain" "this" {
 
   scaling_parameters {
     desired_instance_type = var.instance_type
-    // desired_replication_count = var.instance_count
-    // desired_partition_count   = var.partition_count
+    desired_replication_count = var.instance_count
+    desired_partition_count   = var.partition_count
   }
 }
 
@@ -30,7 +30,6 @@ data "aws_iam_policy_document" "search_suggest" {
 }
 
 resource "aws_cloudsearch_domain_service_access_policy" "this" {
-  id            = local.domain_name
   domain_name   = aws_cloudsearch_domain.this.id
   access_policy = data.aws_iam_policy_document.search_suggest.json
 }
